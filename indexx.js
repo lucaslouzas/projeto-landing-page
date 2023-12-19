d6b96d492d5346f9a5c1e1fa827f1ad8b70d70f2
lucaslouzas <153942736+lucaslouzas@users.noreply.github.com>
2023-12-19 15:00:31 -0300

    Add files via upload

---
+++ indexx.js
@@ -0,0 +1,19 @@
+var SetaDireita = window.document.getElementById("seta-direita")
+var Leonardo = window.document.getElementById("leonardo")
+var Samantha = window.document.getElementById("samantha")
+var Bruna = window.document.getElementById("bruna")
+var SetaEsquerda = window.document.getElementById("seta-esquerda")
+
+function RolarParaDireita( ) {
+    Leonardo.style = "display:none"
+    Bruna.style ="display: flex"
+    SetaDireita.style ="display: none"
+    SetaEsquerda.style = "display: flex; margin-top:125px"
+}
+
+function RolarParaEsquerda( ) {
+    Leonardo.style = "display:flex"
+    Bruna.style ="display: none"
+    SetaDireita.style = "display: flex; margin-top: 125px"
+    SetaEsquerda.style ="display: none"
+}
